9e8fb32b738a877a1729e6ae81cf835fb6ddc2d5
121140111_Bendry Lakburlawal <bendrylakburlawal01@gmail.com>
2024-05-26 17:28:07 +0700

    feat: Admin update movie

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -1,7 +1,8 @@
-export default function Checkbox({ className = "", handleChange, ...props }) {
+export default function Checkbox({ className = "", handleChange, checked = false, ...props }) {
     return (
         <input
             {...props}
+            defaultChecked={checked}
             type="checkbox"
             className={
                 "rounded border-gray-300 text-alerange shadow-sm focus:ring-2 focus:ring-alerange ring-2 ring-alerange" +
